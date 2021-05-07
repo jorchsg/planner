16cb8a058f9dd294ef5e9e70bf55774e718be472
jorchsg <sandoval.jorge.407@gmail.com>
2021-05-06 17:25:22 -0700

    Configurate json server and fetch project data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+          <p>{{project.title}}</p>
+      </div>
+
+    </div>
   </div>
 </template>
 
@@ -9,7 +14,24 @@
 export default {
   name: 'Home',
   components: {
+
+  },
+  data(){
+      return {
+        projects: []
+      }
+    },
+
+    async mounted(){
+      try{
+        const resp = await fetch('http://localhost:3000/projects');
+        const data = await resp.json();
+        console.log(data);
+        return this.projects = data;
+      }catch(err){
+        console.log('Error: ', err)
+      }
+    }
     
-  }
 }
 </script>
